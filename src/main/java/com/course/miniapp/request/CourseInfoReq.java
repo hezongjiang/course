dfd9b31d375e381da32a4f073ce8df4577e827c3
hezongjiang <hezongjiang@shoplineapp.com>
2024-09-05 16:36:16 +0800

    feat：创建课程增加字段

--- src/main/java/com/course/miniapp/request/CourseInfoReq.java
+++ src/main/java/com/course/miniapp/request/CourseInfoReq.java
@@ -6,6 +6,8 @@ import lombok.Data;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Data
 public class CourseInfoReq {
@@ -21,4 +23,10 @@ public class CourseInfoReq {
 
     @NotBlank
     private String userId;
+
+    private List<Integer> weekNum = IntStream.rangeClosed(1, 20).boxed().collect(Collectors.toList());
+
+    private String place;
+
+    private String teacher;
 }
